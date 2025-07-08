1c7288848d2d304c7e04a9659d0737a3b6d2ef19
XingYu-Zhong <1736101137@qq.com>
2025-07-08 15:53:19 +0800

    新增 LaTeX 输出选项配置，包含文档树分组和文档类设置，以支持 Sphinx 文档的 LaTeX 输出。

--- docs/conf.py
+++ docs/conf.py
@@ -37,3 +37,31 @@ source_suffix = {
     '.txt': 'markdown',
     '.md': 'markdown',
 }
+
+# -- Options for LaTeX output ------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-latex-output
+
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
+# Grouping the document tree into LaTeX files. List of tuples
+# (source start file, target name, title, author, documentclass [howto/manual]).
+latex_documents = [
+    ('index', 'coderepoindex.tex', 'coderepoindex Documentation', 'CodeRepoIndex Team', 'manual'),
+]
+
